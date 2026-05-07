7d32635b375c923b40f951df5ba50adf373cd4c6
Bliver <bliver@user.com>
2026-05-08 07:09:14 +0900

    Fix: add missing leaflet.markercluster JS import

--- frontend/src/components/ClusterMarkers.jsx
+++ frontend/src/components/ClusterMarkers.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
+import 'leaflet.markercluster';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
